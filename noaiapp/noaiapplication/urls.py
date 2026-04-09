997b005fc2288fece2e7516934f7d390c3b1ed1f
subba reddy arikatla <subbareddyarikatla8@gmail.com>
2026-04-09 21:54:22 +0530

    new change to studentreview

--- noaiapp/noaiapplication/urls.py
+++ noaiapp/noaiapplication/urls.py
@@ -5,17 +5,4 @@ from . import views
 urlpatterns=[
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('students/',views.student_list,name='student-list'),
-    path('students/<int:pk>/',views.student_details,name='student-list'),
-    path('blukstudents/',views.bulkStudentCreate, name='bulk student create'),
-    path('question/',views.QuestionList,name='question list'),
-    path('question/<int:pk>/',views.QuestionDetails,name='question details'),
-    path('products/', views.get_products),
-    path('create/', views.create_product),
-    path('crash/', views.crash_api),
-    path('update/<int:id>/', views.update_product),
-    path('productsdemo/',views.productdemoList,name='productdemolist'),
-    path('productsdemo/<int:id>/',views.productdemoDetails,name='productdemodetails'),
-    path('customer/',views.Customerlist,name='customerlist'),
-    path('customer/<int:pk>/',views.CustomerDetails,name='customerlist')
 ]
